160a31026035ad8c9e6533cb9fad7ce3edcab75a
Elvis Barrionuevo <idcareg.g@gmail.com>
2021-03-04 18:58:26 -0500

    AvatarPage part 2

--- lib/pages/avatar_page.dart
+++ lib/pages/avatar_page.dart
@@ -1,10 +1,40 @@
-
-
 import 'package:flutter/material.dart';
 
 class AvatarPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Avatar"),
+        actions: [
+          Container(
+            padding: EdgeInsets.all(5.0),
+            child: CircleAvatar(
+              backgroundColor: Colors.greenAccent,
+              child: Text(
+                "A",
+                style: TextStyle(
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+          ),
+          Container(
+            padding: EdgeInsets.all(5.0),
+            child: CircleAvatar(
+              backgroundImage: NetworkImage("https://images.pexels.com/photos/3841338/pexels-photo-3841338.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"),
+            ),
+          ),
+        ],
+      ),
+      body: Center(
+        child: FadeInImage(
+          image: NetworkImage("https://images.pexels.com/photos/3841338/pexels-photo-3841338.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"),
+          placeholder: AssetImage("assets/images/jar-loading.gif"),
+          fadeInDuration: Duration(milliseconds: 200),
+        ),
+      ),
+    );
   }
 }
