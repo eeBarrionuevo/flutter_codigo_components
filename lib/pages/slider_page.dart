6891633438ddd1401f7789963d0cd40f269dbc1f
Elvis Barrionuevo <idcareg.g@gmail.com>
2021-03-05 19:31:03 -0500

    SliderPage part 2

--- lib/pages/slider_page.dart
+++ lib/pages/slider_page.dart
@@ -1,11 +1,29 @@
 import 'package:flutter/material.dart';
 
 class SliderPage extends StatelessWidget {
+
+  List<int> listNumber = [2, 10, 50, 55, 43, 22, 42, 77 , 4, 8];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("Slider Page"),
+      ),
+      body: Container(
+        child: ListView.builder(
+          itemCount: listNumber.length,
+          itemBuilder: (BuildContext context, int index) {
+
+            int item = listNumber[index];
+
+            return FadeInImage(
+              placeholder: AssetImage("assets/images/jar-loading.gif"),
+              image: NetworkImage("https://picsum.photos/500/500/?image=$item"),
+            );
+
+          },
+        ),
       ),
     );
   }
